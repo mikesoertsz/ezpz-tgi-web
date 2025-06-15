6e8f9fbeaf4b723400ac8042df9d7cb561e6862d
zypzapdeveloper <developer.access@zypzap.com>
2025-06-16 00:01:12 +0100

    fixing details

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,12 @@
+import { Wrapper } from "@/components/atoms";
 import { LoginForm } from "@/components/login-form";
 
 export default function LoginPage() {
   return (
-    <div className="bg-brand-bgdark flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+    <Wrapper className="bg-brand-bgdark flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
         <LoginForm />
       </div>
-    </div>
+    </Wrapper>
   );
 }
---
+++ components/Titles.tsx
@@ -0,0 +1,111 @@
+import { cva } from "class-variance-authority";
+import { PreHeading, Heading, SubHeading, Body } from "./atoms";
+
+export type Titles = {
+  icon?: React.ReactNode;
+  image?: string;
+  preheading?: string;
+  heading?:
+    | React.ReactNode[]
+    | React.ReactNode
+    | string
+    | string[]
+    | React.ReactNode;
+  subheading?: string | React.ReactNode | string[];
+  body?: string | React.ReactNode | React.ReactNode[];
+};
+
+const titleStyles = cva("flex w-full max-w-3xl", {
+  variants: {
+    theme: {
+      light: "text-black",
+      dark: "text-white",
+    },
+    orientation: {
+      center:
+        "flex-col items-center justify-center w-full text-center gap-2 mb-12",
+      left: "flex-col items-start justify-start w-full text-left gap-2",
+    },
+  },
+  compoundVariants: [
+    {
+      theme: "light",
+      orientation: "center",
+      class: "flex-col items-center justify-center text-black",
+    },
+    {
+      theme: "light",
+      orientation: "left",
+      class: "flex-col items-start justify-start text-black",
+    },
+    {
+      theme: "dark",
+      orientation: "center",
+      class: "flex-col items-center justify-center",
+    },
+    {
+      theme: "dark",
+      orientation: "left",
+      class: "flex-col items-start justify-start text-gray-200",
+    },
+  ],
+  defaultVariants: {
+    theme: "light",
+    orientation: "center",
+  },
+});
+
+interface TitleBlockProps extends Titles {
+  icon?: React.ReactNode;
+  image?: string;
+  preheading?: string;
+  heading?: string;
+  subheading?: string;
+  body?: string | React.ReactNode;
+  theme: "light" | "dark";
+  orientation: "center" | "left";
+}
+
+export function TitleBlock({
+  preheading,
+  heading,
+  subheading,
+  body,
+  theme = "light",
+  orientation = "center",
+}: TitleBlockProps) {
+  const darkModeStyles = {
+    preHeading: "text-brand-primary", // Example color for PreHeading in dark mode
+    heading: "text-brand-highlight", // Example color for Heading in dark mode
+    body: "text-gray-400", // Example color for Body in dark mode
+  };
+
+  return (
+    <div className={titleStyles({ theme, orientation })}>
+      {preheading && (
+        <PreHeading
+          className={theme === "dark" ? darkModeStyles.preHeading : ""}
+        >
+          {preheading}
+        </PreHeading>
+      )}
+      {heading && (
+        <Heading
+          className={`font-title text-4xl leading-[1.35] md:text-[2.5em] font-medium tracking-tighter md:leading-[4rem] ${
+            theme === "dark" ? darkModeStyles.heading : ""
+          }`}
+        >
+          {heading}
+        </Heading>
+      )}
+      {subheading && (
+        <SubHeading className="py-0 my-0">{subheading}</SubHeading>
+      )}
+      {body && (
+        <Body className={theme === "dark" ? darkModeStyles.body : ""}>
+          {body}
+        </Body>
+      )}
+    </div>
+  );
+}
